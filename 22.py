f35d565449a529647667f1ddbcd8f56d39ea4642
041800 <55594818a@example.com>
2025-11-15 16:28:22 +0800

    112

--- 22.py
+++ 22.py
@@ -1,2 +1,9 @@
-import random
+import sys
+from pathlib import Path
+import marshal
+import dis
+
+data = Path(sys.argv[1]).read_bytes()
+co = marshal.loads(data[16:])
+dis.dis(co)
 print("hello world")
